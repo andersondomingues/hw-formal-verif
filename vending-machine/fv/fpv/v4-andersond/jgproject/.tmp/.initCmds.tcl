f73bdaf6e2621a1f4c52f8941233490db0edf11c
Anderson Domingues <ti.andersondomingues@gmail.com>
2019-10-01 11:21:20 -0300

    removed personal project files

--- vending-machine/fv/fpv/v4-andersond/jgproject/.tmp/.initCmds.tcl
+++
@@ -1,3 +0,0 @@
-@puts "INFO: successfully checked out licenses \"jasper_interactive\" and \"jasper_fpv\"."
-@puts "INFO: reading configuration file \"/users/anderson.domingues/.config/jasper/jaspergold.conf\"."
-@get_message -clear
